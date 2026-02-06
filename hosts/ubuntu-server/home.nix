b091b944dfb03db1f795edaf09936d5dd49b8900
netlex <mrnetlex@gmail.com>
2026-02-06 04:54:17 +0100

    sops hm test

--- hosts/ubuntu-server/home.nix
+++ hosts/ubuntu-server/home.nix
@@ -7,6 +7,35 @@
   
   programs.home-manager.enable = true;
 
+  sops = {
+
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+    validateSopsFiles = false;
+
+    age = {
+      # automatically impost host SSH keys as age keys
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      # this will use an age key that is expected to already be in filesystem
+      keyFile = "/home/netlex/.config/sops/age/keys.txt";
+      # generate a new key if the key specified above does not exist
+      generateKey = true;
+    };
+
+    # secrets will be output to /run/secrets
+    # e.g. /run/secrets/smb-creds
+    secrets = {
+      atuin-session = { 
+		owner = "netlex";
+		mode = "0600";
+      };
+      atuin-key = { 
+      	owner = "netlex";
+      	mode = "0600";
+      };
+    };
+  };
+
   home.packages = with pkgs; [
 	bat
 	btop 
