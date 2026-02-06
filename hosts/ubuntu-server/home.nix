19969452745004ebcc0e0e4c7a7ac56d8a9422a2
netlex <mrnetlex@gmail.com>
2026-02-06 03:34:09 +0100

    declarative fastfetch

--- hosts/ubuntu-server/home.nix
+++ hosts/ubuntu-server/home.nix
@@ -29,6 +29,10 @@
 	zellij
 	zoxide
   ];
+  home.file.".config/fastfetch/config.jsonc".source = ./../../dotfiles/fastfetch/config.jsonc;
+  home.file.".config/fastfetch/full.jsonc".source = ./../../dotfiles/fastfetch/full.jsonc;
+  home.file.".config/fastfetch/simple.jsonc".source = ./../../dotfiles/fastfetch/simple.jsonc;
+  
   programs.nh = {
   	enable = true;
   	flake = "/home/netlex/NixOS";
